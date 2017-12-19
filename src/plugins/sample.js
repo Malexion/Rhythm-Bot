9791dbc3eabe8806ab3c92815a93a0553df8b8a9
Malexion <Malexion@users.noreply.github.com>
2017-12-18 21:57:54 -0600

    Plugin Example

---
+++ src/plugins/sample.js
@@ -0,0 +1,8 @@
+
+module.exports = function(bot) {
+    // Modify bot here
+    // bot.config // Rhythm-Bot Config object
+    // bot.client // discord.js client
+    // bot.queue // Music Queue
+    //console.log(bot);
+};
