cfd55897913b113d19b0194b28adc0bf54d17340
Malexion <Malexion@users.noreply.github.com>
2020-07-10 12:08:48 -0500

    Sort of handle invalid token / bot-config creation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import * as fs from 'fs';
+import { requireFile, writeJson, logger, BotConfig, Bot } from './resources';
 
-import { requireFile, logger, BotConfig, Bot } from './resources';
-
+if (!fs.existsSync('./bot-config.json')) {
+    writeJson({ discord: { token: '<BOT-TOKEN>' } }, './bot-config.json');
+}
 let config: BotConfig = requireFile('./bot-config.json');
+
+if (!!config && config.discord.token === '<BOT-TOKEN>') {
+    logger.debug('Invalid Token - Create valid token in the Discord Developer Portal');
+    console.log('Invalid Token - Create valid token in the Discord Developer Portal');
+    process.exit(0);
+}
 
 let bot = new Bot(config);
 
