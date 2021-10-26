39c954a37a164f4078e4551861a6634d044c2d8d
Louis Orleans <louis@orleans.io>
2021-10-25 23:45:07 -0700

    🙈 ignore migrations

--- migrations/Migration20211014120158.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20211014120158 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table `playlist` (`name` varchar not null, `created_at` datetime not null, `updated_at` datetime not null, `list` json not null, primary key (`name`));');
-  }
-
-}
